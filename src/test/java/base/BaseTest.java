ebf127cf87957a352ddfd4596ce477eace283269
Prabhavathi <prabhavathi.av@gmail.com>
2026-03-26 10:19:08 +0530

    updated beforeclass  logic

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -21,7 +21,7 @@ public class BaseTest {
 
     @BeforeClass
     public void classSetup() {
-        PlaywrightFactory.getPlaywright();
+        playwrightFactory.initPlaywright();
     }
 
     @BeforeMethod
@@ -32,16 +32,20 @@ public class BaseTest {
                 .setSnapshots(true)
                 .setSources(true));
         page.navigate("https://practicesoftwaretesting.com/");
-
     }
 
     @AfterMethod
     public void tearDown(ITestResult result) {
         ScreenshotManager.takeScreenshot("Before_Teardown_" + result.getMethod().getMethodName());
-        String testName=result.getMethod().getMethodName();
-        Path path=Path.of("target/traces", testName + ".zip");
+        String testName = result.getMethod().getMethodName();
+        Path path = Path.of("target/traces", testName + ".zip");
         PlaywrightFactory.getContext().tracing().stop(new Tracing.StopOptions()
                 .setPath(path));
         playwrightFactory.tearDown();
+    }
+
+    @AfterClass
+    public void classTearDown() {
+        playwrightFactory.closePlaywright();
     }
 }
